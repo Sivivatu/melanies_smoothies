dbc55b45ce4c91b541f8b33f173edbdfdc651958
Paul <Sivivatu@users.noreply.github.com>
2024-09-17 12:09:30 +0100

    Update streamlit_app.py
    moving to pandas df for processing

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,8 +17,10 @@ st.write(
 cnx = st.connection('snowflake')
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop()
+# convert snowpark to pandas DF
+pd_df=my_dataframe.to_pandas()
+# st.dataframe(data=my_dataframe, use_container_width=True)
+# st.stop()
 
 name_on_order = st.text_input(
     'Name on Smoothie'
@@ -37,8 +39,10 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        st.subheader(fruit_chosen + ' Nutrition Information')
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+        st.subheader(fruit_chosen + ' Nutrition Information')
         # st.text(fruityvice_response.json())
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
